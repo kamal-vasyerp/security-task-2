a178bb859a1c06f3af0d73a40a09496f0e1590d1
Kamal Jain <kamaljain@vasyerp.com>
2024-02-19 09:55:17 +0530

    task started

---
+++ src/main/java/com/practise/securitytasktwo/SecurityTask2Application.java
@@ -0,0 +1,13 @@
+package com.practise.securitytasktwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTask2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/securitytasktwo/SecurityTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.securitytasktwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
